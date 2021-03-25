38f56cf0e87766f154c46cfc66fde5792dc2b971
999hyeon <999@hyeon.pro>
2021-03-25 12:10:27 +0900

    :sparkles: [제품 페이지] TKFF-280 (#19)

---
+++ Theme/templates/page-tkff-280.php
@@ -0,0 +1,408 @@
+<?php
+
+/**
+ * Template Name: TKFF?-280
+ * Template Post Type: page
+ */
+
+$zeplin = get_home_url() . '/wp-content/uploads/zeplin';
+get_header();
+include('product/hero.php');
+?>
+<main class="pageTemplate tkfe350">
+  <div class="section main">
+    <img class="d-none d-lg-block" draggable="false" src="<?php echo $zeplin ?>/tkff-280-all.jpg" srcset="<?php echo $zeplin ?>/tkff-280-all@2x.jpg 2x, <?php echo $zeplin ?>/tkff-280-all@3x.jpg 3x">
+    <img class="d-block d-lg-none w-100" draggable="false" src="<?php echo $zeplin ?>/1742.jpg" srcset="<?php echo $zeplin ?>/1742@2x.jpg 2x, <?php echo $zeplin ?>/1742@3x.jpg 3x">
+    <div class="container">
+      <div class="imgWrap">
+        <img draggable="false" src="<?php echo $zeplin ?>/28183.png" srcset="<?php echo $zeplin ?>/28183@2x.png 2x, <?php echo $zeplin ?>/28183@3x.png 3x">
+      </div>
+      <div class="body">
+        <h1>성장 단계에 따른 토출량 조절</h1>
+        <strong>푸고 프리미엄 뉴트럴 시리즈 유아용 텀블러</strong>
+        <p>TKFF-280</p>
+      </div>
+    </div>
+  </div>
+  <?php get_template_part('template-parts/header/product') ?>
+  <div class="section section1">
+    <div class="container">
+      <div class="header">
+        <h1>COLOR · SIZE</h1>
+        <p>색상 및 용량</p>
+      </div>
+      <img draggable="false" src="<?php echo $zeplin ?>/3027.png" srcset="<?php echo $zeplin ?>/3027@2x.png 2x, <?php echo $zeplin ?>/3027@3x.png 3x">
+      <img draggable="false" src="<?php echo $zeplin ?>/209.png" srcset="<?php echo $zeplin ?>/209@2x.png 2x, <?php echo $zeplin ?>/209@3x.png 3x">
+    </div>
+  </div>
+  <?php
+  echo sectionHero();
+  ?>
+  <div class="section section3">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-xl-6 px-0">
+          <img draggable="false" src="<?php echo $zeplin ?>/123.jpg" srcset="<?php echo $zeplin ?>/123@2x.jpg 2x, <?php echo $zeplin ?>/123@3x.jpg 3x">
+        </div>
+        <div class="col-12 col-xl-6 px-0">
+          <img draggable="false" src="<?php echo $zeplin ?>/124.jpg" srcset="<?php echo $zeplin ?>/124@2x.jpg 2x, <?php echo $zeplin ?>/124@3x.jpg 3x">
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="section section4">
+    <div class="container">
+      <h1>
+        아이의 빨대컵은 왜<br />
+        <span class="pointBg">진공단열 스테인리스 빨대컵</span>이어야 할까?
+      </h1>
+      <img draggable="false" src="<?php echo $zeplin ?>/2989.png" srcset="<?php echo $zeplin ?>/2989@2x.png 2x, <?php echo $zeplin ?>/2989@3x.png 3x">
+      <div class="body">
+        <h2>대장균 증식을 억제하는 건강한 보틀, 푸고</h2>
+        <p>
+          내 아이를 물 건강을 위해서<br />
+          물컵 속 대장균 관리가 무엇보다 중요합니다.<br />
+          최적의 온도 유지로 대장균 증식을 억제하는<br />
+          진공단열 스테인리스 보틀, 푸고<br />
+          엄마아빠의 마음으로 만든 건강한 물컵입니다.
+        </p>
+      </div>
+      <div class="body">
+        <img class="icon" draggable="false" src="<?php echo $zeplin ?>/27052.png" srcset="<?php echo $zeplin ?>/27052@2x.png 2x, <?php echo $zeplin ?>/27052@3x.png 3x">
+        <p>
+          아이가 빨대컵으로 음료를 마실 때 입안의 음식물이 컵 안으로 들어갈 수 있어,<br />
+          유아용 빨대컵 안에서 균이 발생하는 것은 불가피합니다.<br />
+          일정 시간이 지나 아이가 균이 침투된 내용물을 다시 마시게 된다면,<br />
+          아이의 상태에 따라 건강에 악영향을 끼칠 수 있습니다.<br />
+          진공단열 스테인리스 보틀인 푸고는 최적의 온도 유지로<br />
+          여름철 대장균 증식을 억제하는 유아용 스테인리스 보틀입니다.
+        </p>
+      </div>
+      <div class="table">
+        <div class="row m-n1">
+          <div class="col-12 col-lg-2 head">
+            써모스 진공단열<br />
+            스테인리스 보틀
+          </div>
+          <div class="col-12 col-lg-10 imgGroupWrap labelImgWrap">
+            <div class="imgWrap">
+              <img class="icon" draggable="false" src="<?php echo $zeplin ?>/72.png" srcset="<?php echo $zeplin ?>/72@2x.png 2x, <?php echo $zeplin ?>/72@3x.png 3x">
+            </div>
+            <div class="imgWrap">
+              <img class="icon" draggable="false" src="<?php echo $zeplin ?>/73.png" srcset="<?php echo $zeplin ?>/73@2x.png 2x, <?php echo $zeplin ?>/73@3x.png 3x">
+            </div>
+            <div class="imgWrap">
+              <img class="icon" draggable="false" src="<?php echo $zeplin ?>/74.png" srcset="<?php echo $zeplin ?>/74@2x.png 2x, <?php echo $zeplin ?>/74@3x.png 3x">
+            </div>
+            <div class="imgWrap">
+              <img class="icon" draggable="false" src="<?php echo $zeplin ?>/75.png" srcset="<?php echo $zeplin ?>/75@2x.png 2x, <?php echo $zeplin ?>/75@3x.png 3x">
+            </div>
+          </div>
+        </div>
+        <div class="row m-n1">
+          <div class="col-12 col-lg-2 head">플라스틱 보틀</div>
+          <div class="col-12 col-lg-10 imgGroupWrap">
+            <div class="imgWrap">
+              <img class="icon" draggable="false" src="<?php echo $zeplin ?>/76.png" srcset="<?php echo $zeplin ?>/76@2x.png 2x, <?php echo $zeplin ?>/76@3x.png 3x">
+            </div>
+            <div class="imgWrap">
+              <img class="icon" draggable="false" src="<?php echo $zeplin ?>/77.png" srcset="<?php echo $zeplin ?>/77@2x.png 2x, <?php echo $zeplin ?>/77@3x.png 3x">
+            </div>
+            <div class="imgWrap">
+              <img class="icon" draggable="false" src="<?php echo $zeplin ?>/78.png" srcset="<?php echo $zeplin ?>/78@2x.png 2x, <?php echo $zeplin ?>/78@3x.png 3x">
+            </div>
+            <div class="imgWrap">
+              <img class="icon" draggable="false" src="<?php echo $zeplin ?>/79.png" srcset="<?php echo $zeplin ?>/79@2x.png 2x, <?php echo $zeplin ?>/79@3x.png 3x">
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="src">출처: 써모스 자체 조사</div>
+    </div>
+  </div>
+  <div class="section section5">
+    <div class="container">
+      <div class="header">
+        <h1>
+          상상력과 오감을 자극하는<br />
+          자연스러운 디자인
+        </h1>
+        <p>
+          지구의 자연을 연상시키는 얼씨톤의 컬러를 포인트로 사용하고 마개와 버튼 모양을 변경하여 <br />
+          아이들이 직접 손으로 만지며 창의력을 높일 수 있는 NEW 디자인으로 만들었어요.<br />
+          꽃, 과일, 구름 등 자연을 닮은 그래픽에 표정을 넣어 아이와 눈 맞춤 하는 푸고를 만나보세요.
+        </p>
+      </div>
+      <div class="row">
+        <div class="col-12 px-0">
+          <img draggable="false" src="<?php echo $zeplin ?>/543.jpg" srcset="<?php echo $zeplin ?>/543@2x.jpg 2x, <?php echo $zeplin ?>/543@3x.jpg 3x">
+        </div>
+      </div>
+      <div class="row pink pb-0">
+        <div class="col-6 imgCol">
+          <img draggable="false" src="<?php echo $zeplin ?>/227.png" srcset="<?php echo $zeplin ?>/227@2x.png 2x, <?php echo $zeplin ?>/227@3x.png 3x">
+        </div>
+        <div class="col-6 contentCol">
+          <h1>
+            나오는 음료 양을<br />
+            조절할 수 있는 마개
+          </h1>
+          <p>
+            마개 안쪽에 음료가 나오는 양을 3단계로 조절할 수 있는 <br />
+            토출량 조절판이 있어 아이가 음료를 흘리지 않고 마실 수 있게 <br />
+            도와준답니다.
+          </p>
+        </div>
+      </div>
+      <div class="row three">
+        <div class="col-6 imgCol">
+          <img draggable="false" src="<?php echo $zeplin ?>/3055.png" srcset="<?php echo $zeplin ?>/3055@2x.png 2x, <?php echo $zeplin ?>/3055@3x.png 3x">
+        </div>
+        <div class="col-6 contentCol">
+          <h1>
+            성장단계에 맞춰<br />
+            상호 호환 가능<span class="comment">(*별도구매)</span>
+          </h1>
+          <p>
+            푸고 프리미엄 뉴트럴 시리즈는 아이의 성장단계에 맞추어 소모품을<br />
+            자유롭게 변경할 수 있는데요.<br />
+            시리즈를 톤온톤 배색으로 디자인하여 호환해 사용해도 잘 어울려요<br />
+            바닥 커버도 시리즈 내 호환이 가능해<br />
+            원하는 컬러로 변경하며 사용할 수 있어요.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="section section6">
+    <div class="container">
+      <div class="row">
+        <div class="col-5">
+          <img draggable="false" src="<?php echo $zeplin ?>/3035.png" srcset="<?php echo $zeplin ?>/3035@2x.png 2x, <?php echo $zeplin ?>/3035@3x.png 3x">
+        </div>
+        <div class="col-7 contentCol">
+          <div class="title">
+            원터치 더블액션 버튼
+          </div>
+          <p>
+            한 손가락으로 버튼을 누르면 열리는 쉬운 개폐 방식으로 뚜껑이 열리면서<br />
+            빨대를 통해 물이 뿜어져 나오는 것도 막아줘요.
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-7 contentCol align-items-end text-end">
+          <div class="title">
+            새지 않아 안심
+          </div>
+          <p>
+            뚜껑을 닫으면 내용물이 새지 않아 물샘 걱정 없이 외출할 수 있어요.
+          </p>
+        </div>
+        <div class="col-5">
+          <img draggable="false" src="<?php echo $zeplin ?>/3036.png" srcset="<?php echo $zeplin ?>/3036@2x.png 2x, <?php echo $zeplin ?>/3036@3x.png 3x">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-5">
+          <img draggable="false" src="<?php echo $zeplin ?>/3008.png" srcset="<?php echo $zeplin ?>/3008@2x.png 2x, <?php echo $zeplin ?>/3008@3x.png 3x">
+        </div>
+        <div class="col-7 contentCol">
+          <div class="title">
+            취향에 맞춰 사용하는 그립
+          </div>
+          <p>
+            바디링, 손잡이 두 가지 그립을 취향에 따라 자유롭게 사용해보세요.
+          </p>
+          <ul class="list">
+            <li>
+              링 타입으로 부피를 최소화해 휴대성이 뛰어나요.
+            </li>
+            <li>
+              손잡이가 돌아가지 않고 아이가 쉽게 잡을 수 있어 빨대컵을<br />
+              떨어트릴 염려 없이 편하게 사용할 수 있어요.
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-7 contentCol align-items-end text-end">
+          <div class="title">빨대컵을 보호하는 실리콘 바닥 커버</div>
+          <p>
+            제품이 미끄러지지 않게 막아주고, 가벼운 충격으로부터 제품을 보호해 줘요.
+          </p>
+        </div>
+        <div class="col-5">
+          <img draggable="false" src="<?php echo $zeplin ?>/3039.png" srcset="<?php echo $zeplin ?>/3039@2x.png 2x, <?php echo $zeplin ?>/3039@3x.png 3x">
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-5">
+          <img draggable="false" src="<?php echo $zeplin ?>/3010.png" srcset="<?php echo $zeplin ?>/3010@2x.png 2x, <?php echo $zeplin ?>/3010@3x.png 3x">
+        </div>
+        <div class="col-7 contentCol">
+          <div class="title">분리 가능한 음용구</div>
+          <p>
+            음용구 패킹이 작아져 마개에서 분리가 쉬워졌어요. <br />
+            간단하게 분리되어 구석구석 손쉽게 세척할 수 있고 마개는 식기세척기에 사용할 수 있어요.
+          </p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-7 contentCol align-items-end text-end">
+          <div class="title">
+            진공단열 스테인리스 이중구조
+          </div>
+          <p>
+            스테인리스 이중구조로 음료를 안전하고 신선하게 보관이 가능해요.
+          </p>
+        </div>
+        <div class="col-5">
+          <img draggable="false" src="<?php echo $zeplin ?>/3040.png" srcset="<?php echo $zeplin ?>/3040@2x.png 2x, <?php echo $zeplin ?>/3040@3x.png 3x">
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-5">
+          <img draggable="false" src="<?php echo $zeplin ?>/3012.png" srcset="<?php echo $zeplin ?>/3012@2x.png 2x, <?php echo $zeplin ?>/3012@3x.png 3x">
+        </div>
+        <div class="col-7 contentCol">
+          <div class="title">안전한 소재로 안심</div>
+          <p>
+            본체 내외 병은 모두 SUS304 레벨의 스테인리스를 사용하고 있으며,<br />
+            모든 재질은 식품위생법에서 정한 유해물질 검사에서 합격한 재질로 안전하게 사용할 수 있어요.
+          </p>
+        </div>
+      </div>
+
+    </div>
+  </div>
+  <div class="section section7">
+    <div class="container-fluid px-0">
+      <div class="row mx-0">
+        <div class="col-6 px-0">
+          <img draggable="false" src="<?php echo $zeplin ?>/883.jpg" srcset="<?php echo $zeplin ?>/883@2x.jpg 2x, <?php echo $zeplin ?>/883@3x.jpg 3x">
+        </div>
+        <div class="col-6 px-0">
+          <img draggable="false" src="<?php echo $zeplin ?>/893.jpg" srcset="<?php echo $zeplin ?>/893@2x.jpg 2x, <?php echo $zeplin ?>/893@3x.jpg 3x">
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="header">
+        <h2>
+          경제적으로<br />
+          사용할 수 있어요
+        </h2>
+        <p>
+          푸고 소모품은 별도 구매가 가능합니다.<br />
+          구성품을 잃어버렸거나, 소모품 사용 주기가 끝났다면,<br />
+          소모품만 교체하여 푸고 사용 주기를 늘려보세요.
+        </p>
+      </div>
+      <div class="body">
+        <div class="item" style="background-image: url(<?php echo $zeplin ?>/182@3x.png)">
+          <span>손잡이</span>
+        </div>
+        <div class="item" style="background-image: url(<?php echo $zeplin ?>/183@3x.png)">
+          <span>바디링</span>
+        </div>
+        <div class="item" style="background-image: url(<?php echo $zeplin ?>/184@3x.png)">
+          <span>바닥커버</span>
+        </div>
+        <div class="item" style="background-image: url(<?php echo $zeplin ?>/213@3x.png)">
+          <span>패킹세트</span>
+        </div>
+        <div class="item" style="background-image: url(<?php echo $zeplin ?>/212@3x.png)">
+          <span>텀블러타입 마개</span>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="section8">
+    <div class="container">
+      <div class="header">
+        <h2>
+          써모스의<br />
+          품질관리 프로세스
+        </h2>
+        <p>
+          전수검사 2번, 추출검사 2번 실시!<br />
+          오래 걸리더라도, <br />
+          믿을 수 있는 제품을 만들고 있습니다.
+        </p>
+      </div>
+      <div class="body">
+        <ul>
+          <li>
+            <span>
+              스테인리스<br />
+              재료
+            </span>
+          </li>
+          <li>
+            <span>
+
+              금속 프레스<br />
+              가공
+            </span>
+          </li>
+          <li>
+            <span>
+              진공 열처리
+            </span>
+          </li>
+          <li class="active">
+            <span>
+              전수검사<br />
+              시행
+            </span>
+          </li>
+          <li>
+            <span>
+              에이징
+            </span>
+          </li>
+          <li class="active">
+            <span>
+              전수검사<br />
+              시행
+            </span>
+          </li>
+          <li>
+            <span>
+              도장/연마/<br />
+              인쇄
+            </span>
+          </li>
+          <li class="active">
+            <span>
+              추출검사<br />
+              시행
+            </span>
+          </li>
+          <li>
+            <span>
+              조립
+            </span>
+          </li>
+          <li class="active">
+            <span>
+              추출검사<br />
+              시행
+            </span>
+          </li>
+          <li>
+            <span>
+              제품 출하
+            </span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</main>
+<?php
+get_footer();
