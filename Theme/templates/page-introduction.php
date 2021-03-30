b2ce8fda375521498f6e694605b9438220879fac
Hansanghyeon <999@hyeon.pro>
2021-03-30 16:36:39 +0900

    :construction: #issuecomment-809989104

--- Theme/templates/page-introduction.php
+++ Theme/templates/page-introduction.php
@@ -15,76 +15,110 @@ get_header();
   </div>
   <div class="section section1">
     <div class="container">
-      <h1>푸고는<br class="d-block d-xl-none" /> 생각했습니다</h1>
-      <p>
-        매일 옷을 갈아입는 하늘을, <br />
-        어제는 팔랑이던 나비구름이<br class="d-block d-xl-none" /> 오늘은 솜사탕 구름이 되고,<br />
-        푸르던 새싹이<br class="d-block d-xl-none" /> 눈송이 같은 하얀꽃이 되는,<br />
-        아이들의 상상력과 오감을 자극하는<br class="d-block d-xl-none" /> 자연을 보여주고 싶은 마음
-      </p>
-      <p>
-        푸고 프리미엄 뉴트럴 시리즈의 시작입니다
-      </p>
+      <div class="row">
+        <div class="col-12">
+          <h1>푸고는<br class="d-block d-xl-none" /> 생각했습니다</h1>
+          <p>
+            매일 옷을 갈아입는 하늘을, <br />
+            어제는 팔랑이던 나비구름이<br class="d-block d-xl-none" /> 오늘은 솜사탕 구름이 되고,<br />
+            푸르던 새싹이<br class="d-block d-xl-none" /> 눈송이 같은 하얀꽃이 되는,<br />
+            아이들의 상상력과 오감을 자극하는<br class="d-block d-xl-none" /> 자연을 보여주고 싶은 마음
+          </p>
+          <p>
+            푸고 프리미엄 뉴트럴 시리즈의 시작입니다
+          </p>
+        </div>
+      </div>
     </div>
   </div>
   <div class="section section2">
-    <img class="desktop" draggable="false" src="<?php echo $zeplin ?>/2943.png" srcset="<?php echo $zeplin ?>/2943@2x.png 2x, <?php echo $zeplin ?>/2943@3x.png 3x">
+    <div class="text-end">
+      <img class="desktop d-none d-md-inline-block" draggable="false" src="<?php echo $zeplin ?>/2943.png" srcset="<?php echo $zeplin ?>/2943@2x.png 2x, <?php echo $zeplin ?>/2943@3x.png 3x">
+      <img class="mobile d-inline-block d-md-none" draggable="false" src="<?php echo $zeplin ?>/011.png" srcset="<?php echo $zeplin ?>/011@2x.png 2x, <?php echo $zeplin ?>/011@3x.png 3x">
+    </div>
     <div class="container">
-      <h1>처음으로<br class="d-block d-xl-none" /> 만나는 푸고</h1>
-      <p>
-        소중한 우리 아이들에게<br class="d-block d-xl-none" /> Earthy Neutrals의<br class="d-none d-xl-block" />
-        자연을<br class="d-block d-xl-none" /> 보여주고 싶은 마음을 담았습니다.<br />
-        자연을 담은<br class="d-block d-xl-none" /> 푸고 프리미엄 뉴트럴 시리즈를<br class="d-block d-xl-none" /> 만나보세요.
-      </p>
+      <div class="row">
+        <div class="col-12">
+          <h1>처음으로<br class="d-block d-xl-none" /> 만나는 푸고</h1>
+          <p>
+            소중한 우리 아이들에게<br class="d-block d-xl-none" /> Earthy Neutrals의<br class="d-none d-xl-block" />
+            자연을<br class="d-block d-xl-none" /> 보여주고 싶은 마음을 담았습니다.<br />
+            자연을 담은<br class="d-block d-xl-none" /> 푸고 프리미엄 뉴트럴 시리즈를<br class="d-block d-xl-none" /> 만나보세요.
+          </p>
+        </div>
+      </div>
     </div>
   </div>
   <div class="section section3">
-    <img class="desktop d-none d-md-block" draggable="false" src="<?php echo $zeplin ?>/2944.png" srcset="<?php echo $zeplin ?>/2944@2x.png 2x, <?php echo $zeplin ?>/2944@3x.png 3x">
-    <img class="desktop d-block d-md-none" draggable="false" src="<?php echo $zeplin ?>/260.png" srcset="<?php echo $zeplin ?>/260@2x.png 2x, <?php echo $zeplin ?>/260@3x.png 3x">
+    <img class="desktop d-none d-md-inline-block" draggable="false" src="<?php echo $zeplin ?>/2944.png" srcset="<?php echo $zeplin ?>/2944@2x.png 2x, <?php echo $zeplin ?>/2944@3x.png 3x">
+    <img class="desktop d-inline-block d-md-none" draggable="false" src="<?php echo $zeplin ?>/260.png" srcset="<?php echo $zeplin ?>/260@2x.png 2x, <?php echo $zeplin ?>/260@3x.png 3x">
     <div class="container">
-      <h1>손으로<br class="d-block d-xl-none" /> 느끼는 푸고</h1>
-      <p>
-        순수함, 포근함, 달콤함, 부드러움의 </br>
-        [Soft touch]가 모티브가 되어</br>
-        아이들이 작은 손으로 직접 느낄 수 있는 </br>
-        푸고 프리미엄 뉴트럴 시리즈를 만들었습니다.
-      </p>
+      <div class="row">
+        <div class="col-12">
+          <h1>손으로<br class="d-block d-xl-none" /> 느끼는 푸고</h1>
+          <p>
+            순수함, 포근함, 달콤함, 부드러움의 </br>
+            [Soft touch]가 모티브가 되어</br>
+            아이들이 작은 손으로 직접 느낄 수 있는 </br>
+            푸고 프리미엄 뉴트럴 시리즈를 만들었습니다.
+          </p>
+        </div>
+      </div>
     </div>
   </div>
   <div class="section section4">
-    <img class="desktop" draggable="false" src="<?php echo $zeplin ?>/2945.png" srcset="<?php echo $zeplin ?>/2945@2x.png 2x, <?php echo $zeplin ?>/2945@3x.png 3x">
+    <div class="text-end">
+      <img class="desktop d-none d-md-inline-block" draggable="false" src="<?php echo $zeplin ?>/2945.png" srcset="<?php echo $zeplin ?>/2945@2x.png 2x, <?php echo $zeplin ?>/2945@3x.png 3x">
+      <img class="mobile d-inline-block d-md-none" draggable="false" src="<?php echo $zeplin ?>/2542.png" srcset="<?php echo $zeplin ?>/2542@2x.png 2x, <?php echo $zeplin ?>/2542@3x.png 3x">
+    </div>
     <div class="container">
-      <h1>아이와<br class="d-block d-xl-none" /> 눈맞춤 하는 푸고</h1>
-      <p>
-        꽃, 과일, 구름 등 자연을 닮은 그래픽에<br class="d-block d-xl-none" /> 귀여운 표정을<br class="d-none d-xl-block" />
-        넣은 푸고 캐릭터는<br class="d-block d-xl-none" /> 아이가 물을 마실 때 캐릭터와 <br />
-        눈을 마주치며 인사할 수 있도록<br class="d-block d-xl-none" /> 섬세하게 디자인되었어요.
-      </p>
+      <div class="row">
+        <div class="col-12">
+          <h1>아이와<br class="d-block d-xl-none" /> 눈맞춤 하는 푸고</h1>
+          <p>
+            꽃, 과일, 구름 등 자연을 닮은 그래픽에<br class="d-block d-xl-none" /> 귀여운 표정을<br class="d-none d-xl-block" />
+            넣은 푸고 캐릭터는<br class="d-block d-xl-none" /> 아이가 물을 마실 때 캐릭터와 <br />
+            눈을 마주치며 인사할 수 있도록<br class="d-block d-xl-none" /> 섬세하게 디자인되었어요.
+          </p>
+        </div>
+      </div>
     </div>
   </div>
   <div class="section section5">
     <div class="group">
-      <img class="desktop path2946" draggable="false" src="<?php echo $zeplin ?>/2946.png" srcset="<?php echo $zeplin ?>/2946@2x.png 2x, <?php echo $zeplin ?>/2946@3x.png 3x">
+      <img class="desktop d-none d-md-inline-block path2946" draggable="false" src="<?php echo $zeplin ?>/2946.png" srcset="<?php echo $zeplin ?>/2946@2x.png 2x, <?php echo $zeplin ?>/2946@3x.png 3x">
+      <img class="mobile d-inline-block d-md-none" draggable="false" src="<?php echo $zeplin ?>/2552.png" srcset="<?php echo $zeplin ?>/2552@2x.png 2x, <?php echo $zeplin ?>/2552@3x.png 3x">
       <img class="desktop path252" draggable="false" src="<?php echo $zeplin ?>/252.png" srcset="<?php echo $zeplin ?>/252@2x.png 2x, <?php echo $zeplin ?>/252@3x.png 3x">
     </div>
     <div class="container">
-      <h1>건강을 생각한 푸고</h1>
-      <p>
-        본체 내외 병은 모두 SUS304 레벨의 스테인리스를<br />
-        사용하고 있으며, 모든 재질은<br class="d-block d-xl-none" /> 식품위생법에서 정한<br class="d-none d-xl-block" />
-        유해물질 검사에서<br class="d-block d-xl-none" /> 합격한 재질로 안전하게 사용할 수 있어요
-      </p>
+      <div class="row">
+        <div class="col-12">
+          <h1>건강을 생각한 푸고</h1>
+          <p>
+            본체 내외 병은 모두 SUS304 레벨의 스테인리스를<br />
+            사용하고 있으며, 모든 재질은<br class="d-block d-xl-none" /> 식품위생법에서 정한<br class="d-none d-xl-block" />
+            유해물질 검사에서<br class="d-block d-xl-none" /> 합격한 재질로 안전하게 사용할 수 있어요
+          </p>
+        </div>
+      </div>
     </div>
   </div>
   <div class="section section6">
-    <img class="desktop" draggable="false" src="<?php echo $zeplin ?>/2947.png" srcset="<?php echo $zeplin ?>/2947@2x.png 2x, <?php echo $zeplin ?>/2947@3x.png 3x">
+    <div class="text-end">
+      <img class="desktop d-none d-md-inline-block" draggable="false" src="<?php echo $zeplin ?>/2947.png" srcset="<?php echo $zeplin ?>/2947@2x.png 2x, <?php echo $zeplin ?>/2947@3x.png 3x">
+      <img class="mobile d-inline-block d-md-none" draggable="false" src="<?php echo $zeplin ?>/2562.png" srcset="<?php echo $zeplin ?>/2562@2x.png 2x, <?php echo $zeplin ?>/2562@3x.png 3x">
+    </div>
     <div class="container">
-      <h1>직원들의 마음을<br class="d-block d-xl-none" /> 담은 푸고</h1>
-      <p>
-        푸고 프리미엄 뉴트럴 시리즈는<br class="d-block d-xl-none" /> 아이를 키우는 <br class="d-none d-xl-block" />
-        자사 직원들이<br class="d-block d-xl-none" /> 직접 제품 개발에 참여하여 육아를 통해 <br />
-        얻은 경험을 담아 만들었어요.
-      </p>
+      <div class="row">
+        <div class="col-12">
+          <h1>직원들의 마음을<br class="d-block d-xl-none" /> 담은 푸고</h1>
+          <p>
+            푸고 프리미엄 뉴트럴 시리즈는<br class="d-block d-xl-none" /> 아이를 키우는 <br class="d-none d-xl-block" />
+            자사 직원들이<br class="d-block d-xl-none" /> 직접 제품 개발에 참여하여 육아를 통해 <br />
+            얻은 경험을 담아 만들었어요.
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </main>
