8fdce8d5b7b2f963c86b8e723b0d75493508db4a
Hansanghyeon <999@hyeon.pro>
2021-05-07 00:07:07 +0900

    :sparkles: 1차 피드백 수정사항 반영완료

--- Theme/templates/product/detail.php
+++ Theme/templates/product/detail.php
@@ -1,13 +1,12 @@
 <?php
 function detail($arg)
 {
-  $zeplin = get_home_url() . '/wp-content/uploads/zeplin';
-  if ($arg === 'three') {
-    return <<<HTML
+    $zeplin = get_home_url() . '/wp-content/uploads/zeplin';
+    if ($arg === 'three') {
+        return <<<HTML
     <div class="row three d-none d-md-flex">
       <div class="col-12 col-lg-6 imgCol">
-        <img class="d-none d-lg-block" draggable="false" src="$zeplin/3055.png" srcset="$zeplin/3055@2x.png 2x, $zeplin/3055@3x.png 3x">
-        <img class="d-block d-lg-none" draggable="false" src="$zeplin/236.png" srcset="$zeplin/236@2x.png 2x, $zeplin/236@3x.png 3x">
+        <img draggable="false" src="$zeplin/3055.png" srcset="$zeplin/3055@2x.png 2x, $zeplin/3055@3x.png 3x">
       </div>
       <div class="col-12 col-lg-6 contentCol">
         <h1>
@@ -15,11 +14,13 @@ function detail($arg)
           상호 호환 가능<span class="comment">(*별도구매)</span>
         </h1>
         <p>
-          푸고 프리미엄 뉴트럴 시리즈는 아이의 성장단계에 맞추어 소모품을<br class="d-none d-lg-block" />
-          자유롭게 변경할 수 있는데요.<br class="d-none d-lg-block" />
-          시리즈를 톤온톤 배색으로 디자인하여 호환해 사용해도 잘 어울려요.<br class="d-none d-lg-block" />
+          푸고 프리미엄 뉴트럴 시리즈는<br class="d-none d-lg-block" />
+          아이의 성장단계에 맞추어<br class="d-none d-lg-block" />
+          소모품을 자유롭게 변경할 수 있는데요.<br class="d-none d-lg-block" />
+          시리즈를 톤온톤 배색으로 디자인하여<br class="d-none d-lg-block" />
+          호환해 사용해도 잘 어울려요.<br class="d-none d-lg-block" />
           바닥 커버도 시리즈 내 호환이 가능해<br class="d-none d-lg-block" />
-          원하는 컬러로 변경하며 사용할 수 있어요.
+          원하는 컬러로 변경하며 사용할 수 있어요.<br class="d-none d-lg-block" />
         </p>
       </div>
     </div>
@@ -27,9 +28,9 @@ function detail($arg)
       <img draggable="false" src="$zeplin/tkfd200-sec05-03.png" srcset="$zeplin/tkfd200-sec05-03@2x.png 2x, $zeplin/tkfd200-sec05-03@3x.png 3x">  
     </div>
 HTML;
-  }
-  if ($arg === 'threejnx') {
-    return <<<HTML
+    }
+    if ($arg === 'threejnx') {
+        return <<<HTML
     <div class="row three jnx d-none d-md-flex">
       <div class="col-12 col-lg-6 imgCol">
         <img class="d-none d-lg-block" draggable="false" src="$zeplin/3017.png" srcset="$zeplin/3017@2x.png 2x, $zeplin/3017@3x.png 3x">
@@ -50,5 +51,5 @@ HTML;
       <img draggable="false" src="$zeplin/jnx501k-sec05-02.png" srcset="$zeplin/jnx501k-sec05-02@2x.png 2x, $zeplin/jnx501k-sec05-02@3x.png 3x">  
     </div>
 HTML;
-  }
+    }
 }
